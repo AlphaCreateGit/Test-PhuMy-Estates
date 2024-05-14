c19e124fc9c648d1b265ebbb811dc2b7f167b65e
Gary <gary.hai@namtech.sg>
2024-05-14 14:24:55 +0700

    Update page MBTT

--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
 $( document ).ready(function() {
     scrollHeader();
+    animationImageBanner();
 });
 function scrollHeader() {
     let height = $('.header').height();
@@ -35,4 +36,20 @@ function scrollHeader() {
 
     // Re-initialize ScrollTrigger when page is refreshed
     $(window).on('load', initializeScrollTrigger);
+}
+function animationImageBanner(){
+    var $bannerImage = $(".banner__image");
+    var $locations = $(".location.d1, .location.d3, .location.d4");
+    var $locate = $(".detail__locate");
+    $(".location.d2").hover(function () {
+        $bannerImage.toggleClass("result_hover");
+        $(".detail__d2").toggleClass("show");
+        $locate.toggleClass("change");
+    });
+
+    $(".detail__d2").hover(function () {
+        $bannerImage.toggleClass("result_hover");
+        $locations.toggleClass("hover");
+        $locate.toggleClass("change");
+    });
 }
